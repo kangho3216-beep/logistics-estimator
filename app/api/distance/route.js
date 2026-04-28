c8d9ae5234b465cc5720a33a1992e22bda4654a8
kangho3216-beep <kangho3216@gmail.com>
2026-04-28 20:05:54 +0900

    Create route.js

---
+++ app/api/distance/route.js
@@ -0,0 +1,36 @@
+export async function POST(req) {
+  const { origin, destinations } = await req.json();
+
+  const API_KEY = "여기에_너_API키";
+
+  const results = [];
+
+  for (let dest of destinations) {
+    const res = await fetch("https://routes.googleapis.com/directions/v2:computeRoutes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": API_KEY,
+      },
+      body: JSON.stringify({
+        origin: { address: origin },
+        destination: { address: dest },
+        travelMode: "DRIVE",
+      }),
+    });
+
+    const data = await res.json();
+
+    const distance =
+      data.routes?.[0]?.distanceMeters
+        ? (data.routes[0].distanceMeters / 1000).toFixed(1) + " km"
+        : "에러";
+
+    results.push({
+      destination: dest,
+      distance,
+    });
+  }
+
+  return Response.json(results);
+}
